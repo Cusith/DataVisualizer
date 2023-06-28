2c70b51856ae7cb8674afb9502256a55d021c862
D:Steve <35042304+Cusith@users.noreply.github.com>
2023-06-27 19:13:39 -0700

    Update DataVisualizer.py
    Added functionality:
    Adjusting Figure Size
    Customizing Colors
    Adding data labels
    Rotating X-axis labels
    Adding a legend
    Saving the graph

--- DataVisualizer.py
+++ DataVisualizer.py
@@ -1,14 +1,30 @@
 import matplotlib.pyplot as plt
 
 def visualize_data(x_labels, data_values):
+    # Adjust the figure size
+    plt.figure(figsize=(8, 6))
+
     # Create a bar chart using Matplotlib
-    plt.bar(x_labels, data_values)
+    colors = ['red', 'green', 'blue']  # Custom colors for bars
+    bars = plt.bar(x_labels, data_values, color=colors)
     
+    # Add data labels
+    for bar in bars:
+        height = bar.get_height()
+        plt.text(bar.get_x() + bar.get_width() / 2, height, str(height), ha='center', va='bottom')
+
     # Customize the chart
     plt.xlabel('Categories')
     plt.ylabel('Values')
     plt.title('Data Visualization')
-    
+    plt.xticks(rotation=45)  # Rotate x-axis labels
+
+    # Add a legend
+    plt.legend(bars, x_labels)
+
+    # Save the chart as an image file
+    plt.savefig('bar_chart.png')
+
     # Display the chart
     plt.show()
 
